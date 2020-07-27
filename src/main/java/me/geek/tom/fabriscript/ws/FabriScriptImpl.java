25b83e157846354141169cebe325c4975ca136b3
Tom_The_Geek <tgbryan39@gmail.com>
2020-07-27 20:37:13 +0100

    * Add licence header. * Implement API additions * NOT TESTED

--- src/main/java/me/geek/tom/fabriscript/ws/FabriScriptImpl.java
+++ src/main/java/me/geek/tom/fabriscript/ws/FabriScriptImpl.java
@@ -1,7 +1,27 @@
+/*
+ * FabriScript
+ * Copyright (C) 2020 Tom_The_Geek
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package me.geek.tom.fabriscript.ws;
 
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import me.geek.tom.fabriscript.script.ScriptExecutor;
+import me.geek.tom.fabriscript.script.timer.ScriptTimerGroup;
+import me.geek.tom.fabriscript.server.ApiPlayer;
 import me.geek.tom.fabriscript.server.IFabriScript;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
@@ -15,7 +35,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FabriScriptImpl implements IFabriScript {
 
@@ -75,7 +97,11 @@ public class FabriScriptImpl implements IFabriScript {
         ServerPlayerEntity player = players.get(0);
         ServerCommandSource source = player.getCommandSource();
         try {
-            ScriptExecutor.loadAndExecScript(name, "", source);
+            ScriptTimerGroup timers = ScriptExecutor.loadAndExecScript(name, "", source);
+            if (timers != null)
+                source.sendFeedback(new TranslatableText("fabriscript.script.executed",
+                        name, timers.getTotalTime(), timers.getSetupTime(), timers.getEvalTime())
+                        .styled(s->s.withColor(Formatting.GREEN)), false);
         } catch (CommandSyntaxException e) {
             source.sendError(Texts.toText(e.getRawMessage()));
             if (e.getInput() != null && e.getCursor() >= 0) {
@@ -87,16 +113,22 @@ public class FabriScriptImpl implements IFabriScript {
 
                 msg.append(e.getInput().substring(Math.max(0, i - 10), i));
                 if (i < e.getInput().length()) {
-                    Text text = (new LiteralText(e.getInput().substring(i))).formatted(new Formatting[]{Formatting.RED, Formatting.UNDERLINE});
+                    Text text = (new LiteralText(e.getInput().substring(i))).formatted(Formatting.RED, Formatting.UNDERLINE);
                     msg.append(text);
                 }
 
-                msg.append((new TranslatableText("command.context.here")).formatted(new Formatting[]{Formatting.RED, Formatting.ITALIC}));
+                msg.append((new TranslatableText("command.context.here")).formatted(Formatting.RED, Formatting.ITALIC));
                 source.sendError(msg);
             }
             return false;
         }
 
         return true;
+    }
+
+    @Override
+    public List<ApiPlayer> getPlayerList() {
+        return server.getPlayerManager().getPlayerList().stream()
+                .map(player -> new ApiPlayer(player.getUuid(), player.getName().asString())).collect(Collectors.toList());
     }
 }
